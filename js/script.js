88e558156ff75dca6b0da6800656e4aadebe27ff
SabinaKurbanova <kurbanovasabina27@gmail.com>
2022-08-27 23:29:33 +0800

    add js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-document.querySelector(".box__btn").addEventListener("click", function(){
-  var ekran = document.querySelector(".box__ekran");
-  console.log(typeof parseInt(ekran.value));
-  ekran.value = parseInt(ekran.value) + 1;
-});
+var ekran = document.querySelector(".box__ekran");
+let displayTitle = document.querySelector(".display__title")
 
+document.querySelector(".box__btn").addEventListener("click", function(){
+  
+  ekran.value = parseInt(ekran.value) + 1;
+  count()
+});
+function count(){
+  let ekranValue = ekran.value
+  if (ekranValue <=33 ){
+    displayTitle.innerHTML = "subhanallah"
+  }
+  if (ekranValue >=33 && ekranValue <= 66 ){
+    displayTitle.innerHTML = "elhamdülillah"
+  }
+  if (ekranValue >=66 && ekranValue <=99 ){
+    displayTitle.innerHTML = "Allahu Ekber"
+  }
+  if (ekranValue == 100){
+    displayTitle.innerHTML="";
+    ekran.value = 0;
+  }
+}
 document.querySelector(".box__btn-clear").addEventListener("click",function(){
-  document.querySelector(".box__ekran").value = 0;
+  let name1 = ekran.value;
+  name1.value = 0;
+  displayTitle.innerHTML=""
 });
 
 
